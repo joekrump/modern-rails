a9d63b46a8ae082ae962e72dab7e563551f3b1a8
Joe Krump <joe.krump@clio.com>
2022-03-08 10:55:44 -0500

    Upgrade to Rails 7

--- app/frontend/controllers/hello-controller.ts
+++ app/frontend/controllers/hello-controller.ts
@@ -3,6 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   connect() {
     console.log("Hello from Stimulus!")
-    this.element.textContent = "Greetings from a Stimulus controller written in TypeScript"
+    this.element.textContent = "Greetings from a Stimulus controller written in TypeScript!"
   }
 }
